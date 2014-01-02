55fb266830f2baa70584fdc951e17ff9a288b463
RaymanFX <raymanfx@gmail.com>
2014-01-02 11:31:35 +0100

    add msm8660 support
    * straight copy and paste from msm8960

--- Android.mk
+++ Android.mk
@@ -6,8 +6,14 @@ ifneq ($(filter msm8226,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8226 based platforms
     include $(call all-named-subdir-makefiles,msm8x26)
 else
+ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8960 based platforms
     include $(call all-named-subdir-makefiles,msm8960)
+ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
+    #This is for 8660 based platforms
+    include $(call all-named-subdir-makefiles,msm8660)
+endif
+endif
 endif
 endif
 
